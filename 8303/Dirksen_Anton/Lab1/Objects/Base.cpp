0454aa947194b124fd46c57c0dc5244ee5842463
mensch25 <antondir@mail.ru>
2020-04-06 10:38:56 +0600

    lab2 init

---
+++ 8303/Dirksen_Anton/Lab1/Objects/Base.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mensch on 4/6/20.
+//
+
+#include "Base.h"
---
+++ 8303/Dirksen_Anton/Lab1/Objects/Base.h
@@ -0,0 +1,14 @@
+//
+// Created by mensch on 4/6/20.
+//
+
+#ifndef OOP_LABS_BASE_H
+#define OOP_LABS_BASE_H
+
+
+class Base {
+
+};
+
+
+#endif //OOP_LABS_BASE_H
